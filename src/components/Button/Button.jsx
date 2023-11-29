20167dd03de4cfd8dcf0f191d9aa5da6d9408407
Maryan Smoliy <112328727+smoliymaryan89@users.noreply.github.com>
2023-11-29 23:44:06 +0200

    feat: create custom button

---
+++ src/components/Button/Button.jsx
@@ -0,0 +1,20 @@
+import PropTypes from "prop-types";
+
+const Button = ({ title, className, type }) => {
+  return (
+    <button
+      className={`py-[14px] px-[44px] text-[14px] text-white font-semibold outline-none leading-[1.43] w-[136px] bg-blue rounded-[12px]  ${className}`}
+      type={type}
+    >
+      {title}
+    </button>
+  );
+};
+
+Button.propTypes = {
+  title: PropTypes.string.isRequired,
+  className: PropTypes.string,
+  type: PropTypes.string.isRequired,
+};
+
+export default Button;
