66cc00bec9555755f1081f426e55bbe24deb2ab5
Maryan Smoliy <112328727+smoliymaryan89@users.noreply.github.com>
2023-11-30 18:31:10 +0200

    feat: add transition for button

--- src/components/Button/Button.jsx
+++ src/components/Button/Button.jsx
@@ -3,7 +3,7 @@ import PropTypes from "prop-types";
 const Button = ({ title, className, type }) => {
   return (
     <button
-      className={`py-[14px] px-[44px] text-[14px] text-white font-semibold outline-none leading-[1.43] w-[136px] bg-blue rounded-[12px]  ${className}`}
+      className={`py-[14px] px-[44px] text-[14px] text-white font-semibold outline-none leading-[1.43] w-[136px] bg-blue rounded-[12px] transition-colors duration-350 hover:bg-hover ${className}`}
       type={type}
     >
       {title}
