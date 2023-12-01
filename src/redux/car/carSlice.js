c28f5727e01fddbd1a94b6bb3fbe390b2e82eb7c
Maryan Smoliy <112328727+smoliymaryan89@users.noreply.github.com>
2023-12-01 21:56:52 +0200

    feat: create car slice

---
+++ src/redux/car/carSlice.js
@@ -0,0 +1,75 @@
+import { createSlice } from "@reduxjs/toolkit";
+import { getAllCars, getCarByPage } from "./carOperations";
+import { handleFulfilled, handlePending, handleRejected } from "../handlers";
+
+const initialState = {
+  cars: {
+    items: [],
+    allCars: [],
+    favoriteCars: [],
+    isLoading: false,
+    error: null,
+  },
+
+  filter: {
+    carBrand: "",
+    price: "",
+    from: "",
+    to: "",
+  },
+};
+
+const carSlice = createSlice({
+  name: "cars",
+  initialState,
+  reducers: {
+    getFavorites(state) {
+      const storedFavorites = localStorage.getItem("favorites");
+      if (storedFavorites) {
+        state.cars.favoriteCars = JSON.parse(storedFavorites);
+      }
+    },
+    toggleFavorites(state, { payload }) {
+      const favoriteCar = state.cars.allCars.find(({ id }) => id === payload);
+
+      if (!favoriteCar) {
+        return;
+      }
+
+      const isExist = state.cars.favoriteCars.findIndex(
+        ({ id }) => id === favoriteCar.id
+      );
+
+      if (isExist === -1) {
+        state.cars.favoriteCars.push(favoriteCar);
+      } else {
+        state.cars.favoriteCars.splice(isExist, 1);
+      }
+
+      localStorage.setItem(
+        "favorites",
+        JSON.stringify(state.cars.favoriteCars)
+      );
+    },
+    setFilter(state, { payload }) {
+      state.filter = { ...state.filter, ...payload };
+      state.cars.items = [];
+    },
+  },
+  extraReducers: (builder) => {
+    builder
+      .addCase(getCarByPage.fulfilled, (state, { payload }) => {
+        state.cars.items.push(...payload);
+
+        handleFulfilled(state);
+      })
+      .addCase(getAllCars.fulfilled, (state, { payload }) => {
+        state.cars.allCars = payload;
+      })
+      .addCase(getCarByPage.pending, handlePending)
+      .addCase(getCarByPage.rejected, handleRejected);
+  },
+});
+
+export const { getFavorites, toggleFavorites, setFilter } = carSlice.actions;
+export const carReducer = carSlice.reducer;
