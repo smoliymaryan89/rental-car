7035874c89632ec217f35855164342bb5c21cd82
Maryan Smoliy <112328727+smoliymaryan89@users.noreply.github.com>
2023-12-02 18:53:57 +0200

    update: update btn

--- src/components/Button/Button.jsx
+++ src/components/Button/Button.jsx
@@ -1,10 +1,11 @@
 import PropTypes from "prop-types";
 
-const Button = ({ title, className, type }) => {
+const Button = ({ title, className, type, onClick }) => {
   return (
     <button
       className={`py-[14px] px-[44px] text-[14px] text-white font-semibold outline-none leading-[1.43] w-[136px] bg-blue rounded-[12px] transition-colors duration-350 hover:bg-hover ${className}`}
       type={type}
+      onClick={onClick}
     >
       {title}
     </button>
@@ -15,6 +16,7 @@ Button.propTypes = {
   title: PropTypes.string.isRequired,
   className: PropTypes.string,
   type: PropTypes.string.isRequired,
+  onClick: PropTypes.func,
 };
 
 export default Button;
