ad31c86f4327d453ab4d8506cf431e3197cb53e2
Maryan Smoliy <112328727+smoliymaryan89@users.noreply.github.com>
2023-12-02 18:54:39 +0200

    update: update container

--- src/components/Container/Container.jsx
+++ src/components/Container/Container.jsx
@@ -1,11 +1,16 @@
 import PropTypes from "prop-types";
 
-const Container = ({ children }) => {
-  return <div className="w-[1213px] px-[15px] mx-auto">{children}</div>;
+const Container = ({ children, className }) => {
+  return (
+    <div className={`w-[1213px] px-[15px] mx-auto ${className}`}>
+      {children}
+    </div>
+  );
 };
 
 Container.propTypes = {
   children: PropTypes.node.isRequired,
+  className: PropTypes.string,
 };
 
 export default Container;
