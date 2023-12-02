0dd5065b3497b04e6755a2af306e18a52e070dee
Maryan Smoliy <112328727+smoliymaryan89@users.noreply.github.com>
2023-12-02 18:53:43 +0200

    feat: create routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,24 @@
+import { lazy } from "react";
+
+import { Navigate, Route, Routes } from "react-router-dom";
+
+import Layout from "./layouts/Layout";
+
+const HomePage = lazy(() => import("./pages/HomePage"));
+const CatalogPage = lazy(() => import("./pages/CatalogPage"));
+const FavoritesPage = lazy(() => import("./pages/FavoritesPage"));
+
 const App = () => {
-  return <div>App</div>;
+  return (
+    <Routes>
+      <Route path="/" element={<Layout />}>
+        <Route index element={<HomePage />} />
+        <Route path="/catalog" element={<CatalogPage />} />
+        <Route path="/favorites" element={<FavoritesPage />} />
+      </Route>
+      <Route path="*" element={<Navigate to={"/"} />} />
+    </Routes>
+  );
 };
 
 export default App;
